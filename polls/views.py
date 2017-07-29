b74ce9a62b80ca17a748551d84f48fc2dcf847c1
Jason Tham <jtham@sbcglobal.net>
2017-07-29 01:24:25 -0500

    added new views and routes

--- polls/views.py
+++ polls/views.py
@@ -10,3 +10,13 @@ def index(request):
 
 def test(request):
     return HttpResponse("This is a different view.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
