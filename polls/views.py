8b282d7437be2ae00fd9ce33eaa17afbd6a15792
Jason Tham <jtham@sbcglobal.net>
2017-07-29 01:39:21 -0500

    templating

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
+from django.template import loader
 
 from django.shortcuts import render
 from .models import Question
@@ -8,8 +9,11 @@ from .models import Question
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def test(request):
     return HttpResponse("This is a different view.")
